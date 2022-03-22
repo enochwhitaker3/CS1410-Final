a7b4edafe00fe09ac0c5e3135b74732f738b51d4
Enoch Whitaker <enochwhitaker3@gmail.com>
2022-03-22 11:51:19 -0600

    first work commit:

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    public class Merchandise
+    {
+        public string? Brand;
+        public double Price;
+
+    }
+    public class Clothing : Merchandise
+    {
+        
+    }
+    public class Skating: Merchandise
+    {
+
+    }
+    public class Accessories: Merchandise
+    {
+
+    }
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to ACME Skate Shop's Inventory");
+
+        Clothing hat = new Clothing();
+        hat.Brand = "Empyre";
+        Console.WriteLine($"{hat.Brand}");
+        
+    }
+}
