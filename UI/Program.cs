e2f74385051801c5d4c69ea51b8e84386c32b074
Enoch Whitaker <enochwhitaker3@gmail.com>
2022-03-23 09:21:30 -0600

    menu updates

--- UI/Program.cs
+++ UI/Program.cs
@@ -44,12 +44,13 @@ class Program
             this._gender = Gender;
         }
     }
-    public static void menu()
+    public static void mainUI()
     {
         
         Console.Clear();
         Console.WriteLine("Please Enter Your Name");
         string user = Console.ReadLine();
+        Start:
         Console.Clear();
         Console.WriteLine($"Welcome Back Manager {user} to ACME Skate Shop's Inventory!");
         Console.WriteLine("\nWhich Stock Would You Like To Check Today?");
@@ -64,28 +65,79 @@ class Program
         if (userInput == 1)
         {
             Console.Clear();
+            Console.WriteLine("Loading Clothing Merchandise Stock.");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("Loading Clothing Merchandise Stock..");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("Loading Clothing Merchandise Stock...");
+            Thread.Sleep(500);
+            Console.Clear();
             Console.WriteLine("Loading Clothing Merchandise Stock....\n");
+            Thread.Sleep(500);
+            clothingMenu();
+
         }
         else if (userInput == 2)
         {
             Console.Clear();
+            Console.WriteLine("Loading Skateboarding Merchandise Stock.");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("Loading Skateboarding Merchandise Stock..");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("Loading Skateboarding Merchandise Stock...");
+            Thread.Sleep(500);
+            Console.Clear();
             Console.WriteLine("Loading Skateboarding Merchandise Stock....\n");
+            Thread.Sleep(500);
+            skateboardingMenu();
         }
         else if (userInput == 3)
         {
             Console.Clear();
+            Console.WriteLine("Loading Accessorized Merchandise Stock.");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("Loading Accessorized Merchandise Stock..");
+            Thread.Sleep(500);
+            Console.Clear();
+            Console.WriteLine("Loading Accessorized Merchandise Stock...");
+            Thread.Sleep(500);
+            Console.Clear();
             Console.WriteLine("Loading Accessorized Merchandise Stock....\n");
+            Thread.Sleep(500);
+            accessoriesMenu();
         }
         else
         {
             Console.Clear();
             Console.WriteLine("invalid input!\n");
+            Thread.Sleep(1000);
+            goto Start;
         }
     }
-
+    public static void clothingMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("           Clothing Merchandise Stock                  \n");
+    }
+    public static void skateboardingMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("           Skateboarding Merchandise Stock                  \n");
+    }
+    public static void accessoriesMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("           Accessorized Merchandise Stock                  \n");
+    }
     public static void Main()
     {
-        menu();
+        mainUI();
+        
         //Clothing shirt = new Clothing("Empyre", 9.99, "XL", "Black", "Men's");
         //Console.WriteLine($"The {shirt._brand} hat costs {shirt._price} ");
         
