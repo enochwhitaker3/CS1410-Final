f0d28c33af5526f9ff749fa436b51ba32b77abd2
Enoch Whitaker <enochwhitaker3@gmail.com>
2022-03-27 15:12:53 -0600

    interface commit

--- UI/accessoriesUI.cs
+++ UI/accessoriesUI.cs
@@ -6,46 +6,37 @@ namespace accessories
     {
         public static void accessoriesMenu()
         {
-            Console.Clear();
-            Console.WriteLine("           Accessorized Merchandise Stock                  ");
+           Console.Clear();
+            Console.WriteLine("           Accessorized Merchandise Categories                  ");
             Console.WriteLine("\nPlease Pick An Option");
             Console.WriteLine("\n||------------------------------||");
-            Console.WriteLine("|| 1: Check Tops Stock                ||");
+            Console.WriteLine("|| 1: Check Jewlery Stock       ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 2: Check Stickers Stock      ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 3: Check Backpack Stock      ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 4: Check Wallet Stock        ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 5: Check Belt Stock          ||");
             Console.WriteLine("||------------------------------||\n");
-        /*Console.WriteLine("|| 2: Tops Stock                ||");
-        Console.WriteLine("||------------------------------||");
-        Console.WriteLine("|| 3: Bottoms Stock             ||");
-        Console.WriteLine("||------------------------------||");
-        Console.WriteLine("|| 4: Socks Stock               ||");
-        Console.WriteLine("||------------------------------||");
-        Console.WriteLine("|| 5: Shoes Stock               ||");
-        Console.WriteLine("||------------------------------||\n");
-        string _brand = Console.ReadLine();
-        double _price = Convert.ToDouble(Console.ReadLine());
-        string _size = Console.ReadLine();
-        string _color = Console.ReadLine();
-        string _gender = Console.ReadLine();
-        Clothing shirt = new Clothing(_brand, _price, _size, _color, _gender);
-        Console.WriteLine($"Merch: {shirt._brand}");*/
-        Start:
             var userInput = Convert.ToInt32(Console.ReadLine());
             if (userInput == 1)
             {
                 Console.Clear();
-                TopsStock();
+                JewleryStock();
 
             }
             else
             {
-                Console.Clear();
-                Console.WriteLine("invalid input!\n");
-                Thread.Sleep(1000);
-                goto Start;
+
             }
-            static void TopsStock()
-            {
-                Console.WriteLine("hello");
-            }
+
         }
+        static void JewleryStock()
+        {
+            
+        }
+
     }
 }
--- UI/skateboardingUI.cs
+++ UI/skateboardingUI.cs
@@ -7,45 +7,35 @@ namespace skateboarding
         public static void skateboardingMenu()
         {
             Console.Clear();
-            Console.WriteLine("           Skating Merchandise Stock                  ");
+            Console.WriteLine("           Clothing Merchandise Categories                  ");
             Console.WriteLine("\nPlease Pick An Option");
             Console.WriteLine("\n||------------------------------||");
-            Console.WriteLine("|| 1: Check Tops Stock                ||");
+            Console.WriteLine("|| 1: Check Grip Tape Stock     ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 2: Check Decks Stock         ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 3: Check Trucks Stock        ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 4: Check Bearings Stock      ||");
+            Console.WriteLine("||------------------------------||");
+            Console.WriteLine("|| 5: Check Wheel Stock         ||");
             Console.WriteLine("||------------------------------||\n");
-        /*Console.WriteLine("|| 2: Tops Stock                ||");
-        Console.WriteLine("||------------------------------||");
-        Console.WriteLine("|| 3: Bottoms Stock             ||");
-        Console.WriteLine("||------------------------------||");
-        Console.WriteLine("|| 4: Socks Stock               ||");
-        Console.WriteLine("||------------------------------||");
-        Console.WriteLine("|| 5: Shoes Stock               ||");
-        Console.WriteLine("||------------------------------||\n");
-        string _brand = Console.ReadLine();
-        double _price = Convert.ToDouble(Console.ReadLine());
-        string _size = Console.ReadLine();
-        string _color = Console.ReadLine();
-        string _gender = Console.ReadLine();
-        Clothing shirt = new Clothing(_brand, _price, _size, _color, _gender);
-        Console.WriteLine($"Merch: {shirt._brand}");*/
-        Start:
             var userInput = Convert.ToInt32(Console.ReadLine());
             if (userInput == 1)
             {
                 Console.Clear();
-                TopsStock();
+                GriptapeStock();
 
             }
             else
             {
-                Console.Clear();
-                Console.WriteLine("invalid input!\n");
-                Thread.Sleep(1000);
-                goto Start;
+
             }
-            static void TopsStock()
-            {
-                Console.WriteLine("hello");
-            }
+
+        }
+        static void GriptapeStock()
+        {
+
         }
     }
 }
