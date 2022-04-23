3a5fd25b3eb92a30af425ef0aa159f9ccc629162
Enoch Whitaker <enochwhitaker3@gmail.com>
2022-04-23 16:52:39 -0600

    passing tests

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -16,31 +16,31 @@ public class Tests
     public void Test1()
     {
         Clothing cottonTop = new Clothing("Thrasher", 25.99, "XL", "Black", "Men's", 5, Clothing.ClothingType.Tops);
-        Assert.AreEqual(45.99, cottonTop.Price);
+        Assert.AreEqual(25.99, cottonTop.Price);
     }
     [Test]
     public void Test2()
     {
         Skateboarding newBearing = new Skateboarding("Bones", 19.99, "Regulation", 1, Skateboarding.SkateboardType.Bearings);
-        Assert.AreEqual("Reds", newBearing.Brand);
+        Assert.AreEqual("Bones", newBearing.Brand);
     }
 
     [Test]
     public void Test3()
     {
         Accessories newSticker = new Accessories("StickerCO", 4.99, "Plastic", 7, Accessories.AccessoriesType.Stickers);
-        Assert.AreEqual(10, newSticker.Quantity);
+        Assert.AreEqual(7, newSticker.Quantity);
     }
     [Test]
     public void Test4()
     {
         Clothing pants = new Clothing("Imper", 46.99, "XL", "Black", "Women's", 3, Clothing.ClothingType.Bottoms);
-        Assert.AreEqual("Blue", pants.Color);
+        Assert.AreEqual("Black", pants.Color);
     }
     [Test]
     public void Test5()
     {
         Accessories newWallet = new Accessories("ACME", 30.99, "Faux-Leather", 2, Accessories.AccessoriesType.Wallet);
-        Assert.AreEqual(Accessories.AccessoriesType.Backpack, newWallet.Type);
+        Assert.AreEqual(Accessories.AccessoriesType.Wallet, newWallet.Type);
     }
 }
